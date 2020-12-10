47a3e386c6b1e5c9a936fc7f916c20c733344479
onpine <718215627@qq.com>
2020-12-10 14:21:05 +0800

    文章详情

--- src/views/article/index.vue
+++ src/views/article/index.vue
@@ -26,16 +26,53 @@
         :icon="article.is_followed ? '' : 'plus'"
         round
         size="small"
+        :loading="isFollowLoading"
+        @click="onFollow"
         >{{ article.is_followed ? "已关注" : "关注" }}</van-button
       >
     </van-cell>
-    <div class="content markdown-body" v-html="article.content"></div>
+    <div
+      ref="article-content"
+      class="content markdown-body"
+      v-html="article.content"
+    ></div>
+    <!-- 底部区域 -->
+    <div class="article-bottom">
+      <van-button
+        class="comment-btn"
+        type="default"
+        round
+        size="small"
+        @click="isPostShow = true"
+        >写评论</van-button
+      >
+      <van-icon name="comment-o" :info="totalCommentCount" color="#777" />
+      <van-icon
+        :color="article.is_collected ? 'orange' : '#777'"
+        :name="article.is_collected ? 'star' : 'star-o'"
+        @click="onCollect"
+      />
+      <van-icon
+        :color="article.attitude === 1 ? 'hotpink' : '#777'"
+        :name="article.attitude === 1 ? 'good-job' : 'good-job-o'"
+        @click="onLike"
+      />
+      <van-icon name="share" color="#777777"></van-icon>
+    </div>
   </div>
 </template>
 
 <script>
 import './github-markdown.css'
-import { getArticleById } from '@/api/article'
+import {
+  getArticleById,
+  addCollect,
+  deleteCollect,
+  addLike,
+  deleteLike
+} from '@/api/article'
+import { ImagePreview } from 'vant'
+import { addFollow, deleteFollow } from '@/api/user'
 
 export default {
   name: 'ArticleIndex',
@@ -48,7 +85,9 @@ export default {
   },
   data () {
     return {
-      article: {}
+      article: {},
+      isFollowLoading: false,
+      totalCommentCount: 0
     }
   },
   computed: {},
@@ -61,6 +100,64 @@ export default {
     async loadArticle () {
       const { data } = await getArticleById(this.articleId)
       this.article = data.data
+      this.$nextTick(() => {
+        this.handlePreviewImage()
+      })
+    },
+
+    handlePreviewImage () {
+      const articleContent = this.$refs['article-content']
+      const imgs = articleContent.querySelectorAll('img')
+      const imgPaths = []
+      imgs.forEach((img, index) => {
+        imgPaths.push(img.src)
+        img.onclick = function () {
+          ImagePreview({
+            images: imgPaths,
+            startPosition: index
+          })
+        }
+      })
+    },
+
+    async onFollow () {
+      this.isFollowLoading = true
+      if (this.article.is_followed) {
+        await deleteFollow(this.article.aut_id)
+      } else {
+        await addFollow(this.article.aut_id)
+      }
+      this.article.is_followed = !this.article.is_followed
+      this.isFollowLoading = false
+    },
+
+    async onCollect () {
+      this.$toast.loading({
+        message: '操作中...',
+        forbidClick: true
+      })
+      if (this.article.is_collected) {
+        await deleteCollect(this.article.art_id)
+      } else {
+        await addCollect(this.article.art_id)
+      }
+      this.article.is_collected = !this.article.is_collected
+      this.$toast.success(`${this.article.is_collected ? '' : '取消'}收藏成功`)
+    },
+
+    async onLike () {
+      this.$toast.loading({
+        message: '操作中...',
+        forbidClick: true
+      })
+      if (this.article.attitude === 1) {
+        await deleteLike(this.article.art_id)
+        this.article.attitude = -1
+      } else {
+        await addLike(this.article.art_id)
+        this.article.attitude = 1
+      }
+      this.$toast.success(`${this.article.attitude === 1 ? '' : '取消'}收藏成功`)
     }
   }
 }
@@ -99,5 +196,33 @@ ul {
 .markdown-body {
   padding: 14px;
   background-color: #fff;
+}
+.article-bottom {
+  position: fixed;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  box-sizing: border-box;
+  height: 44px;
+  border-top: 1px solid #d8d8d8;
+  background-color: #fff;
+  .comment-btn {
+    width: 141px;
+    height: 23px;
+    border: 1px solid #eeeeee;
+    font-size: 15px;
+    line-height: 23px;
+    color: #a7a7a7;
+  }
+  .van-icon {
+    font-size: 20px;
+    .van-info {
+      font-size: 11px;
+      background-color: #e22829;
+    }
+  }
 }
 </style>
